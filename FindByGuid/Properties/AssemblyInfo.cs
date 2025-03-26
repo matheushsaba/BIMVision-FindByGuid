866fea0f557b7b9f1df1744047bdd1e9ddade3a0
matheushsaba <matheushs15@hotmail.com>
2025-03-25 21:01:56 -0300

    Change assembly name and version

--- FindByGuid/Properties/AssemblyInfo.cs
+++ FindByGuid/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("427ec292-98cc-4348-9fdd-2aeb61f3514b")]
+[assembly: Guid("C8B183E1-E90D-4BD5-B3FE-B43925993247")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
